d7e221a0488a438a0f6f0c53c432fdec998d39b1
milosz1324 <121822258+milosz1324@users.noreply.github.com>
2023-10-30 11:26:50 +0100

    Pierwszy commit

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,32 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+private:
+    double x, y;
+public:
+    Wektor2D()  {
+        x = 0.0;
+        y = 0.0;
+    }
+    Wektor2D(double new_x, double new_y) {
+        x = new_x;
+        y = new_y;
+    }
+    void setX(double new_x) {
+        x = new_x;
+    }
+    void setY(double new_y) {
+        y = new_y;
+    }
+    double getX() {
+        return x;
+    }
+    double getY() {
+        return y;
+    }
+    Wektor2D operator+ (Wektor2D &nowy) {
+        return Wektor2D(x + nowy.x, y + nowy.y);
+    }
+    double operator* (Wektor2D &nowy) {
+        return x * nowy.x +  y * nowy.y;
+    }
 };
